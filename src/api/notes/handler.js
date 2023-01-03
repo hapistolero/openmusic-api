aec79dbe591df7d196229534f6b93971de682a91
hapistolero <ilhamhardanz2507@gmail.com>
2023-01-03 22:41:35 +0700

    menambahkan database postgress

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -12,13 +12,17 @@ class NotesHandler {
         this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this)
     }
 
-    postNoteHandler(request, h){
-        try {
-            this._validator.validateNotePayload(request.payload)
-            const {title = 'untitled', body, tags} = request.payload
-
+   async postNoteHandler(request, h){
     
-            const noteId = this._service.addNote({title, body, tags})
+        try {
+            
+            this._validator.validateNotePayload(request.payload)
+            
+            const {title = 'untitled', body, tags} = request.payload
+            
+            const noteId = await this._service.addNote({title, body, tags})
+        
+           
     
             const response = h.response({
                 status: 'success',
@@ -34,9 +38,9 @@ class NotesHandler {
             if ( error instanceof ClientError){
                 const response = h.response({
                     status: 'fail',
-                    message: error.message
+                    message: error.message,
                 })
-                response.code(error.statusCode)
+                response.code(error.statusCode)                                
                 return response
             }
 
@@ -44,7 +48,7 @@ class NotesHandler {
 
             const response = h.response({
                 status: 'error',
-                message: 'Maaf, terjadi legagalan pada server kami.'
+                message: 'Maaf, terjadi kegagalan pada server kami.'
             });
             response.code(500)
             return response
@@ -53,8 +57,8 @@ class NotesHandler {
        
     }
 
-    getNotesHandler(){
-        const notes = this._service.getNotes()
+   async getNotesHandler(){
+        const notes = await this._service.getNotes()
         return{
             status: 'success',
             data:{
@@ -64,10 +68,10 @@ class NotesHandler {
 
     }
 
-    getNoteByIdHandler(request, h){
+    async getNoteByIdHandler(request, h){
         try {
             const {id} = request.params;
-        const note = this._service.getNoteById(id)
+        const note = await this._service.getNoteById(id)
         return{
             status: 'success',
             data:{
@@ -96,12 +100,12 @@ class NotesHandler {
         
     }
 
-    putNoteByIdHandler(request, h){
+    async putNoteByIdHandler(request, h){
 
         try {
             this._validator.validateNotePayload(request.payload)
             const {id} = request.params
-            this._service.editNoteById(id, request.payload)
+            await this._service.editNoteById(id, request.payload)
 
         return {
             status: 'success',
@@ -133,10 +137,10 @@ class NotesHandler {
 
     }
 
-    deleteNoteByIdHandler(request, h){        
+   async deleteNoteByIdHandler(request, h){        
         try {
             const {id} = request.params;
-            this._service.deleteNoteById(id);
+           await this._service.deleteNoteById(id);
             return{
                 status: 'success',
                 message: 'Catatan berhasil dihapus'
@@ -155,7 +159,7 @@ class NotesHandler {
 
             const response = h.response({
                 status: 'error',
-                message: 'Maaf, terjadi legagalan pada server kami.'
+                message: 'Maaf, terjadi kegagalan pada server kami.'
             });
             response.code(500)
             return response
