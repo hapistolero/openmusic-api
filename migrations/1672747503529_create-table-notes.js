45486537fbbf7edc45824119ce0b31be7d3614cc
hapistolero <ilhamhardanz2507@gmail.com>
2023-01-05 16:54:48 +0700

    finished version 1

--- migrations/1672747503529_create-table-notes.js
+++ migrations/1672747503529_create-table-notes.js
@@ -3,35 +3,6 @@
 exports.shorthands = undefined;
 
 exports.up = pgm => {
-    // pgm.createTable('notes',{
-    //     id: {
-    //         type: 'VARCHAR(50)',
-    //         primaryKey: true,
-    //     },
-    //     title: {
-    //         type: 'TEXT',
-    //         notNull: true,
-    //     },
-    //     body: {
-    //         type: 'TEXT',
-    //         notNull: true,
-    //     },
-    //     tags: {
-    //         type: 'TEXT[]',
-    //         notNull: true,
-    //     },       
-    //        created_at: {
-    //         type: 'TEXT',
-    //         notNull: true,
-    //      },
-    //      updated_at: {
-    //         type: 'TEXT',
-    //         notNull: true,
-    //      },
-        
-
-    // });
-
     pgm.createTable('albums',{
         id: {
             type: 'VARCHAR(50)',
